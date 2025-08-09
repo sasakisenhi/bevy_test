c4903a58d99153ba1e024a67121ffd38a2dfee40
sasaki shin <33shin12@gmail.com>
2025-08-09 15:30:07 +0900

    First Commit

---
+++ src/main.rs
@@ -0,0 +1,27 @@
+// src/main.rs
+
+use bevy::prelude::*;
+use bevy::render::renderer::RenderAdapterInfo;
+
+fn main() {
+    env_logger::init(); 
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_systems(Startup, print_adapter_info)
+        .add_systems(Startup, hello)
+        .run();
+}
+
+fn hello() {
+    println!("Hello, Bevy!");
+}
+
+fn print_adapter_info(adapter_info: Option<Res<RenderAdapterInfo>>) {
+    if let Some(info) = adapter_info {
+        println!("GPU: {}", info.name);
+        println!("Backend: {:?}", info.backend);
+    } else {
+        println!("GPU情報が取得できませんでした");
+    }
+}
+
